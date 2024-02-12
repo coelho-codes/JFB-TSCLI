f3bdf8accbad6b2d6241ba36a2025f36041212c2
Davi Coelho <davicoelho.dev@gmail.com>
2024-02-12 16:38:20 -0300

    Forgot to add the gender field to the Character class

--- Character.java
+++ Character.java
@@ -1,13 +1,15 @@
+//Class representing a Sim
 class Character {
     String name;
     int age;
+    String gender; //male or female
     double height; //in feet
     double weight; //in pounds
     String skinColor; //white, brown or black(for the sake of simplicity)
     String eyesColor; //blue, brown, green
-    String hairSize; //Big(reaching shoulders or below), medium(above the shoulders or short) and short(short to bald)
+    String hairSize; //big(reaching shoulders or below), medium(above the shoulders or short) and short(short to bald)
 
-    Character(String name, int age, double height, double weight, String skinColor, String eyesColor, String hairSize) {
+    Character(String name, int age, String gender, double height, double weight, String skinColor, String eyesColor, String hairSize) {
         this.name = name;
         this.age = age;
         this.height = height;
--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 class Main {
     public static void main(String args[]) {
-        Character david = new Character("David", 22, 5.9, 198, "white", "blue", "short");
-        
+        Character david = new Character("David", 22, "male", 5.9, 198, "white", "blue", "short");
     }
 }
