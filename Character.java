7ca8ff170be84e4911f063b1802de26737e42660
Davi Coelho <davicoelho.dev@gmail.com>
2024-02-12 14:40:23 -0300

    Added a constructor method to the Character class

--- Character.java
+++ Character.java
@@ -3,4 +3,17 @@ class Character {
     int age;
     double height; //in feet
     double weight; //in pounds
+    String skinColor; //white, brown or black(for the sake of simplicity)
+    String eyesColor; //blue, brown, green
+    String hairSize; //Big(reaching shoulders or below), medium(above the shoulders or short) and short(short to bald)
+
+    Character(String name, int age, double height, double weight, String skinColor, String eyesColor, String hairSize) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.skinColor = skinColor;
+        this.eyesColor = eyesColor;
+        this.hairSize = hairSize;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,6 @@
 class Main {
     public static void main(String args[]) {
-        Character david = new Character();
+        Character david = new Character("David", 22, 5.9, 198, "white", "blue", "short");
         
-        david.age = 22;
-        david.height = 5.9;
-        david.weight = 198;
-        david.name = "David"; //This is kind of redundant right now lol
-
-        System.out.print("David says: Hello, I'm " + david.name + "!");
-        System.out.print(" I'm " + david.age + " years old.");
-        System.out.print(" My weight is " + david.weight + " pounds,");
-        System.out.println(" and my height is about " + david.height + "ft.");
-        System.out.println("This could be the start of something great!");
     }
 }
