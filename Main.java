59a646046a3d588e9624791d6d9431d5f723dcb2
Davi Coelho <davicoelho.dev@gmail.com>
2024-03-04 23:41:48 -0300

    Too far gone to know what I did in this commit

--- Main.java
+++ Main.java
@@ -6,7 +6,8 @@ class Main {
         String input;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nWelcome to The Sims in CLI!");
+        System.out.println("\033[H\033[2J"); //Clears the console
+        System.out.println("Welcome to The Sims in CLI!");
         System.out.println("To start the game press 's' on your keyboard. To see the menu for help and informations press 'm'. To quit press 'q'.");
 
         for(;;) { //Sets the initial loop for user interactions
@@ -15,7 +16,7 @@ class Main {
             if(input.equals("q")) {
                 break;
             } else if(input.equals("s")) {
-                main.game(main);
+                main.game(main, scanner);
             } else if(input.equals("m")) {
                 main.menu(scanner, main); //If the user selects q(to quit) in the menu method, they will return to this 'for loop' inside main method(!)
             } else {
@@ -24,7 +25,7 @@ class Main {
         }   
     }
 
-    public void game(Main main) {
+    public void game(Main main, Scanner scanner) {
         System.out.println("\nStarting game.");
         String loading = "loading";
         for(int i = 0; i < 3; i++) {
@@ -39,8 +40,11 @@ class Main {
             }
             loading = "loading";
         }
+
         System.out.println();
-        main.create(); //Returns a character(with it's attributes)
+        System.out.println();
+
+        main.create(scanner); //Returns a character(with it's attributes)
     }
 
     public void menu(Scanner scanner, Main main) {
@@ -56,7 +60,7 @@ class Main {
         for(;;) {
             menuInput = scanner.nextLine();
 
-            if(menuInput.equals("q")) { //this is ugly as hell at the moment :(
+            if(menuInput.equals("q")) { //this is ugly as hell at the moment I KNOW :(
                 break;
             } else if(menuInput.equals("a")) {
                 System.out.println("the game works like this...");
@@ -69,23 +73,49 @@ class Main {
             } else if(menuInput.equals("e")) {
                 System.out.println("the actions work like this");
             } else if(menuInput.equals("s")) {
-                main.game(main);
+                main.game(main, scanner);
             } else {
                 System.out.println("Wrong option... Please provide a valid input.");
             }
         }
     }
 
-    public Character create() {
+    //Simplistic version of this method
+    public Character create(Scanner scanner) {
         String name;
         int age;
         String gender; //male or female
         double height; //in feet
         double weight; //in pounds
         String skinColor; //white, brown or black(for the sake of simplicity)
-        String eyesColor; //blue, brown, green
-        String hairSize; //big(reaching shoulders or below), medium(above the shoulders or short) and short(short to bald)
+        String eyesColor; //blue, brown, green(for the same reason)
+        String hairSize; //big(reaching shoulders or below), medium(above the shoulders or short) and short(short or bald)
 
-        System.out.println("this is inside the create method");
+        System.out.println("Start by creating your character!");
+
+        //I have no idea how I could've messed this up but somehow I did
+        while(true) {
+            System.out.println("Full name: ");
+            name = scanner.nextLine();
+            System.out.println("\nAge: ");
+            age = scanner.nextInt();
+            System.out.println("\nGender: ");
+            gender = scanner.nextLine();
+            System.out.println("\nHeight: ");
+            height = scanner.nextDouble();
+            System.out.println("\nWeight: ");
+            weight = scanner.nextDouble();
+            System.out.println("\nSkin color: ");
+            skinColor = scanner.nextLine();
+            System.out.println("\nEyes color: ");
+            eyesColor = scanner.nextLine();
+            System.out.println("\nHair size: ");
+            hairSize = scanner.nextLine();
+            break;
+        }
+
+        Character character = new Character(name, age, gender, height, weight, skinColor, eyesColor, hairSize);
+
+        return character;
     }
 }
